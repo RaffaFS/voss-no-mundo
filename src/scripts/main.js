b15983bbbbfb1e347daf7a7414d3e084a483523d
Rafael Gonçalves Ribeiro <raffafullstack@gmail.com>
2024-06-25 22:08:23 -0300

    retirando pedaços de código inúteis

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,7 @@ const aqui = document.querySelector('#aqui')
 const diversos = document.querySelector('#diversos')
 const voltar = document.querySelector('#voltar')
 const voltar2 = document.querySelector('#voltar2')
+
 const ctBox1 = document.querySelector('#contentBox1')
 const ctBox2 = document.querySelector('#contentBox2')
 const ctBox3 = document.querySelector('#contentBox3')
@@ -14,7 +15,6 @@ voltar2.addEventListener('click', mainPage)
 function informations(){
     ctBox1.classList.add('hidden')
     ctBox2.classList.remove('hidden')
-    ctBox3.classList.add('hidden2')
 }
 
 function mainPage(){
@@ -25,6 +25,5 @@ function mainPage(){
 
 function projects(){
     ctBox1.classList.add('hidden')
-    ctBox2.classList.add('hidden')
     ctBox3.classList.remove('hidden2')
 }

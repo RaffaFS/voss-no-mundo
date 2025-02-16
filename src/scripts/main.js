05790566dc53d5ba9f8d394089e1da4bfc5ce96f
Rafael Gonçalves Ribeiro <raffafullstack@gmail.com>
2025-02-16 01:22:09 -0300

    adicionando terceira tela e configurando estrutura de botões entre telas

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,21 @@
 const aqui = document.querySelector('#aqui')
 const diversos = document.querySelector('#diversos')
+const depoimentos = document.querySelector('#depoimentos')
 const voltar = document.querySelector('#voltar')
 const voltar2 = document.querySelector('#voltar2')
+const voltar3 = document.querySelector('#voltar3')
 
 const ctBox1 = document.querySelector('#contentBox1')
 const ctBox2 = document.querySelector('#contentBox2')
 const ctBox3 = document.querySelector('#contentBox3')
+const ctBox4 = document.querySelector('#contentBox4')
 
 aqui.addEventListener('click', informations)
-diversos.addEventListener('click', projects)
+diversos.addEventListener('click', tecnica)
+depoimentos.addEventListener('click', depos)
 voltar.addEventListener('click', mainPage)
 voltar2.addEventListener('click', mainPage)
+voltar3.addEventListener('click', tecnica)
 
 function informations(){
     ctBox1.classList.add('hidden')
@@ -20,10 +25,26 @@ function informations(){
 function mainPage(){
     ctBox1.classList.remove('hidden')
     ctBox2.classList.add('hidden')
-    ctBox3.classList.add('hidden2')
+    ctBox3.classList.add('hidden')
+    ctBox4.classList.add('hidden2')
 }
 
-function projects(){
+function tecnica(){
     ctBox1.classList.add('hidden')
-    ctBox3.classList.remove('hidden2')
+    ctBox2.classList.add('hidden')
+    ctBox3.classList.remove('hidden')
+    ctBox4.classList.add('hidden2')
 }
+
+function depos(){
+    ctBox1.classList.add('hidden')
+    ctBox2.classList.add('hidden')
+    ctBox3.classList.add('hidden')
+    ctBox4.classList.remove('hidden2')
+}
+
+document.addEventListener('keydown', (e) => {
+    if(e.key === 'Tab') {
+        e.preventDefault();
+    }
+});
